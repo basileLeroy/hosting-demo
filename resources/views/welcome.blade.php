d4304d4fcdb7bd3206c44b46f95a4203d6387e36
basileLeroy <basile.leroy.jnm@hotmail.com>
2022-09-15 15:08:33 +0200

    Adding favicon to head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="{{ asset('images/Basile_and_Code_Logo.png') }}" />
         <title>Basile's Lair</title>
 
         <!-- Fonts -->
