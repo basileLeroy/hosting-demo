6ab23e7ec2da448a573df7795a21ea9ad5537779
basileLeroy <basile.leroy.jnm@hotmail.com>
2022-09-15 13:56:07 +0200

    change tailwind layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,14 +30,15 @@
                 <img src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9" class="absolute h-full w-full object-cover"/>
                 <div class="inset-0 bg-black opacity-25 absolute">
                     </div>
-                <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+                <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-22 xl:py-22">
                         <div class="w-full font-mono flex flex-col items-center relative z-10">
-                                <h1 class="font-extrabold text-5xl text-center text-white leading-tight mt-4">
-                          You are all alone here
-                                  </h1>
-                                <p class="font-extrabold text-8xl my-44 text-white animate-bounce">
+                    <p class="font-extrabold text-8xl my-22 text-white animate-bounce">
                                         404
                                     </p>
+                                <h1 class="font-extrabold text-5xl text-center text-white leading-tight">
+                          You are all alone here
+                                  </h1>
+                               
                             </div>
                     </div>
         </div>
