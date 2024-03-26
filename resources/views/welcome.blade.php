a04b8f4b059949dda9fd7364424b98a707901fb3
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-03-26 10:16:29 +0100

    Add New Template

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Basile & Code</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
+        <div id="App">
+            <div id="botActive">
+                <h1>Active</h1>
+            </div>
+            <div id="logo">
+                <div id="wave"></div>
+            </div>
+            <div id="version">
+                <p>v.2.0.0</p>
+            </div>
+        </div>
+    </body>
+</html>
