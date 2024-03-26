133186291d7440ba0bb13aa98869ba5f6ea7b5ff
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-03-26 10:44:28 +0100

    Change Favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        {{-- Meta --}}
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Basile & Code</title>
 
-        <!-- Fonts -->
+        {{-- Favicon --}}
+        <link rel="shortcut icon" href="{{asset("icons/favicon.png")}}" type="image/x-icon">
+
+        <!-- Fonts -->        
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        {{-- Styling --}}
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
